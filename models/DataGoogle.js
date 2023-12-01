49d1500c10094e8275b3b8205d111f0799659a6f
Arthit P <144419746+Arthittaya02-07@users.noreply.github.com>
2023-12-01 11:37:43 +0700

    Update DataGoogle.js

--- models/DataGoogle.js
+++ models/DataGoogle.js
@@ -1,31 +1,31 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+// const mongoose = require('mongoose')
+// const Schema = mongoose.Schema
 
-const googleSchema = new Schema({
-    googleID: {
-        type: String,
-        required: true
-    },
-    displayname: {
-        type: String,
-        required: true
-    },
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    // image: {
-    //     type: String
-    // },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-});
+// const googleSchema = new Schema({
+//     googleID: {
+//         type: String,
+//         required: true
+//     },
+//     displayname: {
+//         type: String,
+//         required: true
+//     },
+//     firstName: {
+//         type: String,
+//         required: true
+//     },
+//     lastName: {
+//         type: String,
+//         required: true
+//     },
+//     // image: {
+//     //     type: String
+//     // },
+//     createdAt: {
+//         type: Date,
+//         default: Date.now
+//     }
+// });
 
-const datagoogle = mongoose.model('datagoogle', googleSchema)
-module.exports = datagoogle
+// const datagoogle = mongoose.model('datagoogle', googleSchema)
+// module.exports = datagoogle
